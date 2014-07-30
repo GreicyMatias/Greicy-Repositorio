743cb00aa2c44a7051f35a50db89c9feab447fab
GreicyMatias <greicy_matias@hotmail.com>
2014-07-30 11:15:19 -0300

    exercicios

---
+++ ConsoleApplication1/ConsoleApplication1/Exercicios/Unidade01_Complementares.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Exercicios
+{
+    class Unidade01_Complementares
+    {
+        static void Main1(string[] args)
+        {
+            char asterisco = '*';
+            char espaco = ' ';
+            int contador = 0;
+            for (int i = 0; i < 5; i++ )
+            {
+                contador = 0;
+                Console.WriteLine(espaco);
+                do{
+                    Console.Write(asterisco);
+                    contador++;
+                }while(contador <= i);
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            char asterisco = '*';
+            char espaco = ' ';
+            int contador = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                contador = 0;
+                Console.WriteLine(espaco);
+                do
+                {
+                    Console.Write(asterisco);
+                    contador++;
+                } while (contador <= i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ ConsoleApplication1/ConsoleApplication1/Exercicios/Unidade01_Logica.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Exercicios
+{
+    class Unidade01_Logica
+    {
+        static void Main1(string[] args)
+        {
+            //Imprime Nome
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Greicy Matias");
+            }
+            Console.ReadKey();
+        }
+        static void Main2(string[] args)
+        {
+            //Imprime Numeros
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+        static void Main3(string[] args)
+        {
+            //Imprime Padrão 01
+            Console.WriteLine("Legenda: * = ímpar / ** = par\n");                  
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write(i+"\t");
+                int resto = i % 2;
+                if (resto == 1)
+                {
+                    Console.WriteLine("*");
+                }
+                else
+                {
+                    Console.WriteLine("**");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+        static void Main4(string[] args)
+        {
+            //Imprime Padrão 02
+            for (int i = 1; i <= 100; i++)
+            {
+                int resto = i % 4;
+                if (resto == 0)
+                {
+                    Console.WriteLine("PI");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }      
+    }
+}
